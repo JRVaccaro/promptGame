5f7e6b01ae5e3b91f85b0aab54ccb513ea881d9c
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-14 14:50:35 -0400

    inital commit with begining setup

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+const pokemons = ["Pikachu", "Eevee", "Joltik"]
+
+window.alert("WHO'S THAT POKEMON?!")
+
+const randomPokemons = Math.floor(Math.random() * pokmeons.length);//returns a random character 
+
+
+console.log(randomPokemons)
+
+
+let won = false;
+
+for(let guesses = 3; guesses > 0; guesses--){
+
+
+let guess = window.prompt(`Guess the pokemon in ${guess} guesses!`)
+
+}
