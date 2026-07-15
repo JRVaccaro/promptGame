2b6f1bd98029be493b9915e4b2e5c919b1c98238
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-14 20:51:06 -0400

    added photos

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 
 
-const pokemons = ["Pikachu", "Eevee", "Joltik"]
+const pokemons = ["Pikachu", "Eevee", "Yamper"]
+
+const pokemonImages = {
+    Pikachu: "pika.jpg",
+    Eevee: "eevee.jpg",
+    Yamper: "yamper.jpg"
+};
 
  window.alert("WHO'S THAT POKEMON?!")
 
@@ -9,26 +15,43 @@ const randomPokemons = Math.floor(Math.random() * pokemons.length);//returns a r
 
 
 
-const answer = pokemons[randomPokemons]
+const answer = pokemons[randomPokemons];
+
 console.log("Answer: ", answer)
 
 
 console.log(randomPokemons)
 
+const imageEl = document.getElementById("pokemon-image");
+
+imageEl.src = pokemonImages[answer];
+
+setTimeout(() => {
 
 let won = false;
 
 for(let guesses = 3; guesses > 0; guesses--){
 
 
-let guess = window.prompt(`Guess the pokemon in ${guesses} guesses!`)
+let guess = window.prompt(`Guess the pokemon in ${guesses} guesses!`);
 
 if(guess.toLowerCase() === answer.toLowerCase()){
     won = true;
     window.alert("CORRECT");
     break;
 } else if(guesses > 1){
-    window.alert(`Wrong! you have ${guesses} remaining...`)
+    window.alert(`Wrong! you have ${guesses -1} remaining...`)
 }
 
 }
+
+const appDiv = document.getElementById("app");
+const resultEl = document.createElement("p");
+
+if(won){
+    appDiv.textContent = `You got it! The answer was ${answer}.`;
+
+} else {
+    appDiv.textContent = `Out of guesses! The answer was ${answer}`;
+}
+}, 100);
